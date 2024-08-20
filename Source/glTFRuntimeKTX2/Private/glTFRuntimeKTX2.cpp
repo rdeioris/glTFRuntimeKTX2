efbf5428510b39aabcf9317f953bad3a6d6752a6
rdeioris <roberto.deioris@gmail.com>
2024-08-20 13:48:24 +0200

    added support for bForceAutoDetect ImageConfig

--- Source/glTFRuntimeKTX2/Private/glTFRuntimeKTX2.cpp
+++ Source/glTFRuntimeKTX2/Private/glTFRuntimeKTX2.cpp
@@ -19,7 +19,7 @@ void FglTFRuntimeKTX2Module::StartupModule()
 				return;
 			}
 
-	ImageIndex = Parser->GetJsonExtensionObjectIndex(JsonTextureObject, "KHR_texture_basisu", "source", INDEX_NONE);
+			ImageIndex = Parser->GetJsonExtensionObjectIndex(JsonTextureObject, "KHR_texture_basisu", "source", INDEX_NONE);
 		});
 
 	// check if we have mips
@@ -31,81 +31,87 @@ void FglTFRuntimeKTX2Module::StartupModule()
 				return;
 			}
 
-	// check for used extensions
-	if (!Parser->ExtensionsUsed.Contains("KHR_texture_basisu"))
-	{
-		return;
-	}
+			FString MimeType;
 
-	const FString MimeType = Parser->GetJsonObjectString(JsonImageObject, "mimeType", "");
-	// skip non ktx2 mimeType (can be empty)
-	if (MimeType != "" && MimeType != "image/ktx2")
-	{
-		return;
-	}
+			if (!ImagesConfig.bForceAutoDetect)
+			{
 
-	ktxTexture2* KTX2Texture;
-	KTX_error_code KTXResult;
-	ktx_size_t KTX2Offset;
+				// check for used extensions
+				if (!Parser->ExtensionsUsed.Contains("KHR_texture_basisu"))
+				{
+					return;
+				}
 
-	KTXResult = ktxTexture2_CreateFromMemory(Bytes.GetData(), Bytes.Num(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &KTX2Texture);
-	if (KTXResult != KTX_SUCCESS)
-	{
-		if (MimeType != "" || (MimeType == "" && KTXResult != KTX_UNKNOWN_FILE_FORMAT))
-		{
-			Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to load KTX2 texture");
-		}
-		return;
-	}
+				MimeType = Parser->GetJsonObjectString(JsonImageObject, "mimeType", "");
+				// skip non ktx2 mimeType (can be empty)
+				if (MimeType != "" && MimeType != "image/ktx2")
+				{
+					return;
+				}
+			}
 
-	if (KTX2Texture->numLevels > 0)
-	{
-		EPixelFormat PixelFormat = EPixelFormat::PF_B8G8R8A8;
-		if (ktxTexture2_NeedsTranscoding(KTX2Texture))
-		{
-			KTXResult = ktxTexture2_TranscodeBasis(KTX2Texture, KTX_TTF_BC3_RGBA, 0);
+			ktxTexture2* KTX2Texture;
+			KTX_error_code KTXResult;
+			ktx_size_t KTX2Offset;
+
+			KTXResult = ktxTexture2_CreateFromMemory(Bytes.GetData(), Bytes.Num(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &KTX2Texture);
 			if (KTXResult != KTX_SUCCESS)
 			{
-				ktxTexture_Destroy(ktxTexture(KTX2Texture));
-				Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to transcode KTX2 texture");
+				if (KTXResult != KTX_UNKNOWN_FILE_FORMAT)
+				{
+					Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to load KTX2 texture");
+				}
 				return;
 			}
 
-			PixelFormat = EPixelFormat::PF_DXT5;
-		}
-
-		int32 MipWidth = KTX2Texture->baseWidth;
-		int32 MipHeight = KTX2Texture->baseHeight;
-
-		for (uint32 MipIndex = 0; MipIndex < KTX2Texture->numLevels; MipIndex++)
-		{
-			KTXResult = ktxTexture_GetImageOffset(ktxTexture(KTX2Texture), MipIndex, 0, 0, &KTX2Offset);
-			if (KTXResult != KTX_SUCCESS)
+			if (KTX2Texture->numLevels > 0)
 			{
-				ktxTexture_Destroy(ktxTexture(KTX2Texture));
-				Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to get KTX2 texture offset");
-				return;
+				EPixelFormat PixelFormat = EPixelFormat::PF_B8G8R8A8;
+				if (ktxTexture2_NeedsTranscoding(KTX2Texture))
+				{
+					KTXResult = ktxTexture2_TranscodeBasis(KTX2Texture, KTX_TTF_BC3_RGBA, 0);
+					if (KTXResult != KTX_SUCCESS)
+					{
+						ktxTexture_Destroy(ktxTexture(KTX2Texture));
+						Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to transcode KTX2 texture");
+						return;
+					}
+
+					PixelFormat = EPixelFormat::PF_DXT5;
+				}
+
+				int32 MipWidth = KTX2Texture->baseWidth;
+				int32 MipHeight = KTX2Texture->baseHeight;
+
+				for (uint32 MipIndex = 0; MipIndex < KTX2Texture->numLevels; MipIndex++)
+				{
+					KTXResult = ktxTexture_GetImageOffset(ktxTexture(KTX2Texture), MipIndex, 0, 0, &KTX2Offset);
+					if (KTXResult != KTX_SUCCESS)
+					{
+						ktxTexture_Destroy(ktxTexture(KTX2Texture));
+						Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to get KTX2 texture offset");
+						return;
+					}
+
+					ktx_uint8_t* KTX2ImageData = ktxTexture_GetData(ktxTexture(KTX2Texture)) + KTX2Offset;
+					const int64 ImageSize = ktxTexture_GetImageSize(ktxTexture(KTX2Texture), MipIndex);
+
+					FglTFRuntimeMipMap Mip(TextureIndex);
+					Mip.Width = MipWidth;
+					Mip.Height = MipHeight;
+					Mip.PixelFormat = PixelFormat;
+
+					Mip.Pixels.Append(KTX2ImageData, ImageSize);
+
+					Mips.Add(MoveTemp(Mip));
+
+					MipWidth = FMath::Max(MipWidth / 2, 1);
+					MipHeight = FMath::Max(MipHeight / 2, 1);
+
+				}
 			}
 
-			ktx_uint8_t* KTX2ImageData = ktxTexture_GetData(ktxTexture(KTX2Texture)) + KTX2Offset;
-			const int64 ImageSize = ktxTexture_GetImageSize(ktxTexture(KTX2Texture), MipIndex);
-
-			FglTFRuntimeMipMap Mip(TextureIndex);
-			Mip.Width = MipWidth;
-			Mip.Height = MipHeight;
-			Mip.PixelFormat = PixelFormat;
-
-			Mip.Pixels.Append(KTX2ImageData, ImageSize);
-
-			Mips.Add(MoveTemp(Mip));
-
-			MipWidth = FMath::Max(MipWidth / 2, 1);
-			MipHeight = FMath::Max(MipHeight / 2, 1);
-
-		}
-	}
-
-	ktxTexture_Destroy(ktxTexture(KTX2Texture));
+			ktxTexture_Destroy(ktxTexture(KTX2Texture));
 		});
 
 	// extract ImagePixels
@@ -117,69 +123,69 @@ void FglTFRuntimeKTX2Module::StartupModule()
 				return;
 			}
 
-	// check for used extensions
-	if (!Parser->ExtensionsUsed.Contains("KHR_texture_basisu"))
-	{
-		return;
-	}
-
-	const FString MimeType = Parser->GetJsonObjectString(JsonImageObject, "mimeType", "");
-	// skip non ktx2 mimeType (can be empty)
-	if (MimeType != "" && MimeType != "image/ktx2")
-	{
-		return;
-	}
-
-	ktxTexture2* KTX2Texture;
-	KTX_error_code KTXResult;
-	ktx_size_t KTX2Offset;
-
-	KTXResult = ktxTexture2_CreateFromMemory(CompressedPixels.GetData(), CompressedPixels.Num(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &KTX2Texture);
-	if (KTXResult != KTX_SUCCESS)
-	{
-		if (MimeType != "" || (MimeType == "" && KTXResult != KTX_UNKNOWN_FILE_FORMAT))
-		{
-			Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to load KTX2 texture");
-		}
-		return;
-	}
-
-	if (KTX2Texture->numLevels > 0)
-	{
-		if (ktxTexture2_NeedsTranscoding(KTX2Texture))
-		{
-			KTXResult = ktxTexture2_TranscodeBasis(KTX2Texture, KTX_TTF_RGBA32, 0);
-			if (KTXResult != KTX_SUCCESS)
+			// check for used extensions
+			if (!Parser->ExtensionsUsed.Contains("KHR_texture_basisu"))
 			{
-				ktxTexture_Destroy(ktxTexture(KTX2Texture));
-				Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to transcode KTX2 texture");
 				return;
 			}
-		}
-		KTXResult = ktxTexture_GetImageOffset(ktxTexture(KTX2Texture), 0, 0, 0, &KTX2Offset);
-		if (KTXResult != KTX_SUCCESS)
-		{
+
+			const FString MimeType = Parser->GetJsonObjectString(JsonImageObject, "mimeType", "");
+			// skip non ktx2 mimeType (can be empty)
+			if (MimeType != "" && MimeType != "image/ktx2")
+			{
+				return;
+			}
+
+			ktxTexture2* KTX2Texture;
+			KTX_error_code KTXResult;
+			ktx_size_t KTX2Offset;
+
+			KTXResult = ktxTexture2_CreateFromMemory(CompressedPixels.GetData(), CompressedPixels.Num(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &KTX2Texture);
+			if (KTXResult != KTX_SUCCESS)
+			{
+				if (MimeType != "" || (MimeType == "" && KTXResult != KTX_UNKNOWN_FILE_FORMAT))
+				{
+					Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to load KTX2 texture");
+				}
+				return;
+			}
+
+			if (KTX2Texture->numLevels > 0)
+			{
+				if (ktxTexture2_NeedsTranscoding(KTX2Texture))
+				{
+					KTXResult = ktxTexture2_TranscodeBasis(KTX2Texture, KTX_TTF_RGBA32, 0);
+					if (KTXResult != KTX_SUCCESS)
+					{
+						ktxTexture_Destroy(ktxTexture(KTX2Texture));
+						Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to transcode KTX2 texture");
+						return;
+					}
+				}
+				KTXResult = ktxTexture_GetImageOffset(ktxTexture(KTX2Texture), 0, 0, 0, &KTX2Offset);
+				if (KTXResult != KTX_SUCCESS)
+				{
+					ktxTexture_Destroy(ktxTexture(KTX2Texture));
+					Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to get KTX2 texture offset");
+					return;
+				}
+
+				Width = KTX2Texture->baseWidth;
+				Height = KTX2Texture->baseHeight;
+				PixelFormat = EPixelFormat::PF_B8G8R8A8;
+
+				ktx_uint8_t* KTX2ImageData = ktxTexture_GetData(ktxTexture(KTX2Texture)) + KTX2Offset;
+
+				const int64 ImageSize = Width * Height * 4;
+
+				for (int64 IndexR = 0; IndexR < ImageSize; IndexR += 4)
+				{
+					Swap(KTX2ImageData[IndexR], KTX2ImageData[IndexR + 2]);
+				}
+				UncompressedPixels.Append(KTX2ImageData, ImageSize);
+			}
+
 			ktxTexture_Destroy(ktxTexture(KTX2Texture));
-			Parser->AddError("FglTFRuntimeKTX2Module::StartupModule()", "Unable to get KTX2 texture offset");
-			return;
-		}
-
-		Width = KTX2Texture->baseWidth;
-		Height = KTX2Texture->baseHeight;
-		PixelFormat = EPixelFormat::PF_B8G8R8A8;
-
-		ktx_uint8_t* KTX2ImageData = ktxTexture_GetData(ktxTexture(KTX2Texture)) + KTX2Offset;
-
-		const int64 ImageSize = Width * Height * 4;
-
-		for (int64 IndexR = 0; IndexR < ImageSize; IndexR += 4)
-		{
-			Swap(KTX2ImageData[IndexR], KTX2ImageData[IndexR + 2]);
-		}
-		UncompressedPixels.Append(KTX2ImageData, ImageSize);
-	}
-
-	ktxTexture_Destroy(ktxTexture(KTX2Texture));
 		});
 }
 
